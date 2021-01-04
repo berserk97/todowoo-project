891233b34552e14c3cf9a1d9139993d626176610
Saif Malek <saifad840@gmail.com>
2021-01-04 17:03:36 +0500

    todo functionality

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
+from .forms import TodoForm
+from .models import Todo
+from django.utils import timezone
 
 def home(request):
     return render(request, 'todo/home.html')
@@ -41,16 +44,57 @@ def loginuser(request):
             #after login redirect user to his current todo page
             return redirect('currenttodo')
         
-         
-
-
-def currenttodo(request):
-    return render(request, 'todo/currenttodo.html')
 
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
         return redirect('home')
 
+#Todo views
+def createtodo(request):
+    if request.method == 'GET':
+        return render(request, 'todo/createtodo.html', {'form': TodoForm()})
+
+    else:
+        try:
+            form = TodoForm(request.POST)
+            newtodo = form.save(commit=False)
+            newtodo.user = request.user
+            newtodo.save()
+            return redirect('currenttodo')
+        except ValueError:
+            return render(request, 'todo/createtodo.html', {'form': TodoForm(), 'error': 'Bad data is passed in. Try again.'})
 
 
+def currenttodo(request):
+    todos = Todo.objects.filter(user=request.user, datecomplete__isnull=True)
+    return render(request, 'todo/currenttodo.html', {'todos': todos})
+
+def viewtodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'GET':
+        form = TodoForm(instance=todo)
+        return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form})
+
+    else:
+        try:
+            form = TodoForm(request.POST, instance=todo)
+            form.save()
+            return redirect('currenttodo')
+        except ValueError:
+            return render(request, 'todo/viewtodo.html', {'todo': todo, 'error': 'Bad info is passed in. Try again.'})
+
+
+def completetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.datecomplete = timezone.now()
+        todo.save()
+        return redirect('currenttodo')
+
+
+def deletetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('currenttodo')
